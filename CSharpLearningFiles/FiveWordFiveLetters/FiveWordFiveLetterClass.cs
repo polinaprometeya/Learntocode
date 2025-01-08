4bdbec392c6f25ea95ff77c94c318859a518a52b
Prometeya <polina.knudsen@gmail.com>
2025-01-08 13:37:25 +0100

    FiveWordFiveLetters - workinprogress - v3

--- CSharpLearningFiles/FiveWordFiveLetters/FiveWordFiveLetterClass.cs
+++ CSharpLearningFiles/FiveWordFiveLetters/FiveWordFiveLetterClass.cs
@@ -1,36 +1,37 @@
-﻿internal class FiveWordFiveLetterClass
+﻿using System.Runtime.InteropServices;
+
+internal class FiveWordFiveLetterClass
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        readData();
+
+        try
+        {
+            readData();
+        }   
+        catch (Exception ex)
+        {
+             Console.WriteLine("Error or something: " + ex.Message);
+        }
 
     }
 
 
-    // loading the "perfect" data for first run, this passes the values down to five letterfivewpord first version
+    // loading the "perfect" data for first run, then just the data file we are interested in this passes the values down to function that checks if the words are okay.
     static void readData()
     {
-        string dataAllPerfect = @"C:\\Users\\infi-yoga\\source\\repos\\CSharpLearningFiles\\FiveWordFiveLetters\\dataAllPerfect.txt";
+        string dataAllPerfect = @"dataAllPerfect.txt";
 
         if (File.Exists(dataAllPerfect))
         {
             Console.WriteLine("File found");
-            try
-            {
+            
+            
                 string[] fileContentsArray = File.ReadAllLines(dataAllPerfect);
+                modifiedWordList(fileContentsArray);
+            
 
-                //Console.WriteLine("File content ");
-                //for (var i = 0; i < fileContentsArray.Length; i++)
-                //{ Console.WriteLine($"Line #{i + 1}: " + fileContentsArray[i]);}
-                //List<string> fileContentList = fileContentsArray.ToList();
-
-                theFiveLetters(fileContentsArray);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error or something: " + ex.Message);
-            }
         }
         else
         {
@@ -39,49 +40,103 @@
     }
 
     // soooo what if i combine all i tried to learn into one function? It is easier they say , it will be fun they sey.
-    static void theFiveLetters(string[] fileContentList)
+    static void modifiedWordList(string[] fileContentsArray)
     {
-        List<string> wordsList = new List<string>();
-        Stack<char> alphabetStack = new Stack<char>("abcdefghijklmnopqrstuvwxyz".ToCharArray());
-
+        List<string> modifiedWordList = new List<string>();
+       
 
         //go through the the perfect data one word at the time
-        foreach (string word in fileContentList)
-        {
-            char[] wordArray = word.ToCharArray();
-            int alphabetCount = 26;
-
-            //check each word for repetition and remove all used alphabet characters from the alphabet string. 
-            //Console.WriteLine(word);
-            for (int i = 1; i < 5; i++)
+        foreach (string word in fileContentsArray)
             {
-                if (wordArray[i] == wordArray[i - 1])
+                bool isWordOkay = isFiveLettersWordOkay(word);
+                if (isWordOkay)
                 {
-                    //this is not php and therefore pop needs some help along. 
-                    wordArray[i] = alphabetStack.Pop(); // Pop a new letter from the stack
-                    alphabetCount--;
-                }
-
-            }
-
-            for (int i = 1; i < fileContentList.Length; i++)
-            {
-                if (fileContentList[i] != fileContentList[i - 1])
-                {
-                    // Rebuild the word after modification
-                    string modifiedWord = new string(wordArray);
-                    wordsList.Add(modifiedWord); // Add the word to the list after processing
+                    modifiedWordList.Add(word);
+                    Console.WriteLine(word);
                 }
             }
-        }
 
-        // Output the result
-        Console.WriteLine("Processed Words: ");
-        foreach (var word in wordsList)
-        {
-            Console.WriteLine(word);
-        }
+        Console.WriteLine("The count of unique words is: ", modifiedWordList.Count());
+
+        //List<string> uniqueWordsList = new List<string>();
+        //for (int i = 0; i < modifiedWordList.Count() - 1; i++)
+        //{
+        //    string currentWord = modifiedWordList[i];
+        //    string nextWord = modifiedWordList[i + 1];
+
+        //    bool isunique = isWordUnique(string currentWord, string nextWord);
+        //    if (isunique)
+        //    {
+        //        uniqueWordsList.Add(currentWord);
+        //        Console.WriteLine(currentWord);
+        //    }
+        //}
     }
 
-   
+    //static void uniqueWordList()
+    //{
+    //}
+
+    static bool isFiveLettersWordOkay(string word)
+    {
+        char[] wordArray = word.ToCharArray();
+        bool isWordOkay = false;
+
+        //check each word for repetition and if the characters are unique  add it to new list
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (wordArray[i] != wordArray[i + 1]) isWordOkay = true; isWordOkay = false;
+        }
+
+        return isWordOkay;
+
+    }
+    public bool isWordValid(string word)
+    {
+        if (word.Length == 5) return true; return false;
+    }
+
+    // Products are equal if their names and product numbers are equal.
+    public bool isWordUnique(string currentWord, string nextWord)
+    {
+        string x = currentWord;
+        string y = nextWord;
+
+
+        //Check whether any of the compared objects is null.
+        if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+            return false;
+
+        //Check whether the compared objects reference the same data.
+        if (Object.ReferenceEquals(x, y)) return true; return false;
+
+    }
+
+    public bool areTwoStringThesame(string firstString, string secondString)
+    {
+        bool isSame = true;
+
+        for (int i = 0; i < firstString.Length; i++)
+        {
+            int x;
+
+            for (x = 0; x < secondString.Length; x++)
+            {
+                if (firstString[i] == secondString[x])
+                {
+                    break;
+                }
+            }
+
+            if (x == secondString.Length)
+            {
+                isSame = false;
+                break;
+            }
+        }
+
+        return isSame;
+    }
+
+
 }
